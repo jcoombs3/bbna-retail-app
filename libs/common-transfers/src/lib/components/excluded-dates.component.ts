7a779bb2c13335e6f066e86475afc73ca1227a8d
Benjamin Du Preez <benjaminp@backbase.com>
2022-04-11 21:41:09 -0400

    switched to async validators / observable logic only

--- libs/common-transfers/src/lib/components/excluded-dates.component.ts
+++ libs/common-transfers/src/lib/components/excluded-dates.component.ts
@@ -11,6 +11,8 @@ import { NgbDate, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
 import { registerFormControl } from '../helpers/form-helpers';
 import { triggerHook } from '../helpers/hook-helpers';
 import { ExcludedDatesService } from '../services/excluded-dates.service';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'bb-excluded-dates',
@@ -23,15 +25,15 @@ export class ExcludedDatesComponent implements PaymentFormField, OnInit, OnDestr
   control!: AbstractControl;
   validationControl!: FormControl;
 
-  validationLabel = 'Start on';
+  validationLabel = 'Date';
   validationMessages = [
     { name: 'excludedDate', message: 'Cannot transfer on this day' },
     {
-      name: 'pastDate',
+      name: 'dateLessThanMinDate',
       message: 'Start date cannot be in the past',
     },
     {
-      name: 'futureDate',
+      name: 'dateGreaterThanMaxDate',
       message: 'Start date cannot be this far in the future',
     },
   ];
@@ -50,33 +52,39 @@ export class ExcludedDatesComponent implements PaymentFormField, OnInit, OnDestr
     ) as FormControl;
 
     this.control.setValidators([
-      Validators.required,
-      this.excludedDateService.excludedDateValidator(),
-      this.excludedDateService.futureDateValidator(),
-      this.excludedDateService.pastDateValidator(),
+      Validators.required
     ]);
+    this.control.setAsyncValidators([
+      this.excludedDateService.excludedDateValidator()
+    ])
     this.control.updateValueAndValidity();
     this.validationControl = this.group.controls[this.config?.name] as FormControl;
     triggerHook(PaymentFormFieldHooks.onInit, this);
   }
 
-  markDisabled(ngb: NgbDate) {
-    const { year, month, day } = ngb;
-    const date = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
-    return this.excludedDateService.isDateValid(date);
-  }
+  markDisabled$ = this.excludedDateService.isDateValid$.pipe(
+    map((isDateValidFn) => {
+      return (ngb: NgbDate) => {
+        const { year, month, day } = ngb;
+        const date = `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`;
+        return isDateValidFn(date);
+      }
+    })
+  )
 
-  get minDate(): NgbDateStruct {
-    if(!this.excludedDateService.startDate) return undefined;
-    const today = new Date(this.excludedDateService.startDate);
-    return { year: today.getUTCFullYear(), month: today.getUTCMonth() + 1, day: today.getUTCDate() };
-  }
+  minDate$: Observable<NgbDateStruct> = this.excludedDateService.startDate$.pipe(
+    map((startDate) => {
+      const today = new Date(startDate);
+      return { year: today.getUTCFullYear(), month: today.getUTCMonth() + 1, day: today.getUTCDate() };
+    })
+  )
 
-  get maxDate(): NgbDateStruct {
-    if(!this.excludedDateService.endDate) return undefined;
-    const today = new Date(this.excludedDateService.endDate);
-    return { year: today.getUTCFullYear(), month: today.getUTCMonth() + 1, day: today.getUTCDate() };
-  }
+  maxDate$: Observable<NgbDateStruct> = this.excludedDateService.endDate$.pipe(
+    map((endDate) => {
+      const today = new Date(endDate);
+      return { year: today.getUTCFullYear(), month: today.getUTCMonth() + 1, day: today.getUTCDate() };
+    })
+  )
 
   ngOnDestroy() {
     triggerHook(PaymentFormFieldHooks.onDestroy, this);
--- libs/common-transfers/src/lib/services/excluded-dates.service.ts
+++ libs/common-transfers/src/lib/services/excluded-dates.service.ts
@@ -1,62 +1,37 @@
 import { Injectable } from '@angular/core';
-import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn, AsyncValidatorFn } from '@angular/forms';
 import { RestrictedDatesHttpService, RestrictedDates } from '../../../../transfers-dates-http-ang/src';
-import { take } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class ExcludedDatesService {
-  excludedDates = [];
-  startDate: string;
-  endDate: string;
+  restrictedDates$: Observable<RestrictedDates> = this.restrictedDatesHttpService.restrictedDatesGet().pipe(
+    shareReplay()
+  );
 
-  constructor(private restrictedDatesHttpService: RestrictedDatesHttpService) {
-    this.restrictedDatesHttpService.restrictedDatesGet().pipe(take(1)).subscribe((dates: RestrictedDates) => {
-      this.startDate = dates.startDate;
-      this.endDate = dates.endDate;
-      this.excludedDates = dates.restrictedDates;
-    });
-  }
+  startDate$ = this.restrictedDates$.pipe(map((dates: RestrictedDates) => dates.startDate));
+  endDate$ = this.restrictedDates$.pipe(map((dates: RestrictedDates) => dates.endDate));
+  isDateValid$: Observable<(date: string) => boolean> = this.restrictedDates$.pipe(
+    map((dates) => {
+      return (date: string) => dates.restrictedDates.includes(date);
+    })
+  )
 
-  isDateValid(date: string) {
-    return this.excludedDates.includes(date);
-  }
+  constructor(private restrictedDatesHttpService: RestrictedDatesHttpService) { }
 
-  excludedDateValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
+  excludedDateValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors> | null => {
       const selectedDate = new Date(control.value);
       let month = selectedDate.getUTCMonth() + 1;
       let day = selectedDate.getUTCDate();
       let year = selectedDate.getUTCFullYear();
-      return this.isDateValid(`${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`)
-        ? { excludedDate: { value: control.value } }
-        : null;
+      return this.isDateValid$.pipe(
+       map((validFn: (date: string) => boolean) => {
+        const errors = validFn(`${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`)
+        ? { excludedDate: { value: control.value } } : null;      
+        return errors;
+       }))
     };
-  }
-
-  pastDateValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const selectedDate = new Date(control.value).setHours(0, 0, 0, 0);
-      const startDate = this.convertDateToUTC(new Date(this.startDate)).setHours(0, 0, 0, 0);
-      return selectedDate < startDate ? { pastDate: { value: control.value } } : null;
-    };
-  }
-
-  futureDateValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const selectedDate = new Date(control.value).setHours(0, 0, 0, 0);
-      const endDate = this.convertDateToUTC(new Date(this.endDate)).setHours(0, 0, 0, 0);
-      return endDate < selectedDate ? { futureDate: { value: control.value } } : null;
-    };
-  }
-
-  private convertDateToUTC(date: Date) {
-    return new Date(
-      date.getUTCFullYear(),
-      date.getUTCMonth(),
-      date.getUTCDate(),
-      date.getUTCHours(),
-      date.getUTCMinutes(),
-      date.getUTCSeconds(),
-    );
   }
 }
